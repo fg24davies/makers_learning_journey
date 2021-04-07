7f534f8c49665f488ea843dc7cef61ef2fc2f796
Flora Davies <daviesfgo@gmail.com>
2021-04-07 11:09:30 +0100

    week 6

---
+++ week_6/following_the_flow_and_getting_visibility_in_javascript_es6/fizzbuzz.js
@@ -0,0 +1,23 @@
+class FizzBuzz {
+  play(number) {
+    if (this._isDivisibleBy(15, number)) {
+      return 'FizzBuzz';
+    } else if (this._isDivisibleBy(5, number)) {
+      return 'Buzz';
+    } else if (this._isDivisibleBy(3, number)) {
+      return 'Fizz';
+    } else {
+      return number;
+    }
+  }
+
+  _isDivisibleBy(divisor, number) {
+    return number % divisor === 0;
+  }
+}
+
+const fizzBuzz = new FizzBuzz();
+
+for (let i = 1; i <= 100; i++) {
+  console.log(fizzBuzz.play(i));
+}
---
+++ week_6/javabuzz/src/Javabuzz.js
@@ -0,0 +1,7 @@
+class Javabuzz {
+  
+  isDivisibleByThree(number) {
+    return (number % 3 === 0)
+  }
+
+}
